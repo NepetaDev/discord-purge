6fb64f0c7c3d8cc0b1d76a5c9cbeb02b52d33b19
Liam Gladdy <liam@gladdy.co.uk>
2019-01-23 20:35:50 +0000

    Update index.js

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ async function removeMessages(type, target, user){
 
     for (var i = 0; i < messages.length; i++) {
       await removeMessage(messages[i].channel_id, messages[i].id);
-      await sleep(100);
+      await sleep(200);
       bar.tick();
     }
   }
